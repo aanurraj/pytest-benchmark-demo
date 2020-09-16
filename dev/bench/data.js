a40f44ef5684e71d64d171c086ebf365d2c88f47
github-action-benchmark <github@users.noreply.github.com>
2020-09-16 16:37:39 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 4d36820636566682cee70a82141b6287f8f609da

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1600273971929,
+  "lastUpdate": 1600274259017,
   "repoUrl": "https://github.com/aanurraj/pytest-benchmark-demo",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000001339291391087946",
             "extra": "mean: 2.501811332958799 usec\nrounds: 57143"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "committer": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "distinct": true,
+          "id": "4d36820636566682cee70a82141b6287f8f609da",
+          "message": "updated 20->100",
+          "timestamp": "2020-09-16T22:07:03+05:30",
+          "tree_id": "d358262d8305a8eb5cd32549d010db0a9e8b9fc9",
+          "url": "https://github.com/aanurraj/pytest-benchmark-demo/commit/4d36820636566682cee70a82141b6287f8f609da"
+        },
+        "date": 1600274258626,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_sigmoid_10",
+            "value": 454364.23352914245,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000027372580040151836",
+            "extra": "mean: 2.2008774595499956 usec\nrounds: 33899"
+          },
+          {
+            "name": "bench.py::test_sigmoid_20",
+            "value": 440767.10678769636,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000034857881579120117",
+            "extra": "mean: 2.26877184027634 usec\nrounds: 90910"
           }
         ]
       }
