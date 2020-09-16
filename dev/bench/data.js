ed456f8946efd15f9194fc6e68c93032a70af992
github-action-benchmark <github@users.noreply.github.com>
2020-09-16 16:32:51 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 6f62f43c0b33ae07bd0c37cbed2871c581161663

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1600273971929,
+  "repoUrl": "https://github.com/aanurraj/pytest-benchmark-demo",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "committer": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "distinct": true,
+          "id": "6f62f43c0b33ae07bd0c37cbed2871c581161663",
+          "message": "uodate",
+          "timestamp": "2020-09-16T22:02:14+05:30",
+          "tree_id": "bb1827252ebf4374f5fb22879c8b044254bb18eb",
+          "url": "https://github.com/aanurraj/pytest-benchmark-demo/commit/6f62f43c0b33ae07bd0c37cbed2871c581161663"
+        },
+        "date": 1600273971521,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_sigmoid_10",
+            "value": 392232.4386161995,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000010317168806271204",
+            "extra": "mean: 2.5495086625879577 usec\nrounds: 26955"
+          },
+          {
+            "name": "bench.py::test_sigmoid_20",
+            "value": 399710.39655389887,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000001339291391087946",
+            "extra": "mean: 2.501811332958799 usec\nrounds: 57143"
+          }
+        ]
+      }
+    ]
+  }
+}
